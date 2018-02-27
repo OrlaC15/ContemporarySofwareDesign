25b285a5a66ccc883246d2e8ab101034228b67a5
OrlaC15 <orla.cullen@ucdconnect.ie>
2018-02-27 22:31:12 +0000

    assignment 3

---
+++ COMP47480_Assignment_3/src/Triangle.java
@@ -0,0 +1,27 @@
+
+public class Triangle {
+
+	public Triangle(){
+
+	}
+	public String typeof(int side_a,int side_b,int side_c) {
+		int total_a= side_a*side_b*side_c;
+		
+		String result= null;
+		if(total_a>0 ){
+			if(side_a == side_b && side_b == side_c){
+				result= "Equilateral";
+			}
+			else if(side_a == side_b || side_b == side_c || side_a == side_c){
+				result= "Isosceles";
+			}
+			else {
+				result= "Scalene";
+			}
+		}else{
+			result= "Invalid";
+		}
+return result;	
+}
+
+}
